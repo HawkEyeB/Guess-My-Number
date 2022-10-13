354952a0f57f89c2366574b0a657c122684fe947
Pedro Ingro <pedroingrogg@gmail.com>
2022-10-13 19:05:44 -0300

    Alterations Language

---
+++ scripts/forma-classica.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+const modify = function () {
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+};
+
+const neutral = function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When the guess is null
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+
+    //When the guess is Correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    modify();
+
+    if (highScore < score) {
+      document.querySelector('.highscore').textContent = highScore = score;
+    } else if (highScore >= score) {
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('.totalScore').textContent = totalScore =
+      highScore + score;
+
+    //When guess is to high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is to low
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  neutral();
+});
---
+++ scripts/forma-curta.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+let totalScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const modify = function () {
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+};
+
+const neutral = function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When the guess is null
+  if (!guess) {
+    displayMessage('No Number');
+
+    //When the guess is Correct
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    modify();
+
+    if (highScore < score) {
+      document.querySelector('.highscore').textContent = highScore = score;
+    } else if (highScore >= score) {
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('.totalScore').textContent = totalScore =
+      highScore + score;
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '✈ Too high!' : '⚓ Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('⁉ You lost the game !');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  neutral();
+});
+
+document.querySelector('.language-img').addEventListener('click', function () {
+  document.querySelector('h1').textContent = 'Adivinhe Meu Número';
+  document.querySelector('.between').textContent = 'Entre 1 a 20';
+  document.querySelector('.again').textContent = 'De Novo!';
+  document.querySelector('.message').textContent = 'Comece a adivinhar...';
+});
